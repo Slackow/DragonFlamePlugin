e48a7d3b49f99a72de66088aa102e8b581b0c2db
Kihron <starlicblackhole@gmail.com>
2018-07-27 00:12:46 -0400

    Fixed weird stuff

--- src/main/java/com/dragonflame/main/Main.java
+++ src/main/java/com/dragonflame/main/Main.java
@@ -11,9 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
@@ -221,7 +219,6 @@ public class Main extends JavaPlugin implements Listener {
                 for(OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()){
                     p.sendMessage("" + objective.getScore(offlinePlayer).getScore());
                 }
-<<<<<<< HEAD
                 return true;
             case "fly":
                 if (p.hasPermission("dragonflame.fly")) {
@@ -245,8 +242,6 @@ public class Main extends JavaPlugin implements Listener {
                     p.setFoodLevel(20);
                     p.setSaturation(20);
                 }
-=======
->>>>>>> b6d21e96485fae126346ed5868e75d1df888d7bb
                 return true;
         }
         return false;
